ae15f2c1ac66e6d1772e4c4dd6ee8188d821c46d
nomoney34 <dragostrandafir80@gmail.com>
2022-04-12 15:26:53 +0300

    Swapper

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -176,17 +176,26 @@ contract Swapper {
     
     // IUniswapV2Router01 router = IUniswapV2Router01(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
+    address weth;
+    address xcoin;
+    address router;
+    constructor() {
+        weth = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+        router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+        xcoin = 0x848052231C98DbB712b4cff5704a7CAedE08a0F9;
+    }
+
     function swap() public payable {
         address[] memory path = new address[](2);
-        path[0] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); //WETH
+        path[0] = address(weth); //WETH
 
         console.log(msg.sender);
         console.log(address(msg.sender).balance);
         console.log(IERC20(path[0]).balanceOf(msg.sender));
         
 
-        path[1] = address(0xdAC17F958D2ee523a2206206994597C13D831ec7); //USDT
-        IUniswapV2Router01(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D).swapExactETHForTokens {value: msg.value} (10000, path, msg.sender, block.timestamp + 86400);
+        path[1] = address(xcoin); //XCOIN
+        IUniswapV2Router01(router).swapExactETHForTokens {value: msg.value} (10000, path, msg.sender, block.timestamp + 86400);
 
         console.log("Address %s has balance: %d", msg.sender, address(msg.sender).balance);
     }
--- test/sample-test.js
+++ test/sample-test.js
@@ -4,36 +4,38 @@ const { ethers } = require("hardhat");
 
 
 describe("Swapping to WETH then testing XBank", function () {
-  // it("Swap to WETH", async function () {
-  //   const [owner] = await ethers.getSigners();
+  it("Swap to WETH", async function () {
+    const [owner] = await ethers.getSigners();
 
-  //   const Swapper = await ethers.getContractFactory("Swapper");
-  //   const swapper = await Swapper.deploy();
-  //   await swapper.deployed();
+    const Swapper = await ethers.getContractFactory("Swapper");
+    const swapper = await Swapper.deploy();
+    await swapper.deployed();
+        
+    const swapTx = await swapper.connect(owner).swap({value: parseEther("0.0001")});
+    await swapTx.wait();
 
-  //   const swapTx = await swapper.swap({value: parseEther("1")});
-  //   await swapTx.wait();
+    console.log("Sunt aici x2");
 
     
-  // });
-
-  it("XBank - Stake, Withdraw, Claim, RequestLoan", async function() {
-    const XBank = await ethers.getContractFactory("XBank");
-    const xbank = await XBank.deploy();
-    await xbank.deployed();
-            
-    const stake = await xbank.stake(parseEther("1000"));
-    await stake.wait();
-
-    const requestLoan = await xbank.requestLoan(parseEther("200"));
-    await requestLoan.wait();
-
-    const claim = await xbank.claim();
-    await claim.wait();
-
-    const withdraw = await xbank.withdraw();
-    await withdraw.wait();
   });
+
+  // it("XBank - Stake, Withdraw, Claim, RequestLoan", async function() {
+  //   const XBank = await ethers.getContractFactory("XBank");
+  //   const xbank = await XBank.deploy();
+  //   await xbank.deployed();
+            
+  //   const stake = await xbank.stake(parseEther("1000"));
+  //   await stake.wait();
+
+  //   const requestLoan = await xbank.requestLoan(parseEther("200"));
+  //   await requestLoan.wait();
+
+  //   const claim = await xbank.claim();
+  //   await claim.wait();
+
+  //   const withdraw = await xbank.withdraw();
+  //   await withdraw.wait();
+  // });
 
   
 });
